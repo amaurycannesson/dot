cfe474515e9c690cd23850995f313c9df596147a
Amaury Cannesson <amaury.cannesson@gmail.com>
2022-12-30 22:06:29 +0100

    which-key add debug test binding

--- .config/lvim/lua/user/which-key.lua
+++ .config/lvim/lua/user/which-key.lua
@@ -9,6 +9,7 @@ lvim.builtin.which_key.mappings["r"] = {
 lvim.builtin.which_key.mappings["t"] = {
   name = "Neotest",
   t = { "<CMD>lua require'neotest'.run.run()<CR>", "Run nearest test" },
+  d = { "<CMD>lua require'neotest'.run.run({strategy = 'dap'})<CR>", "Debug nearest test" },
   f = { "<CMD>lua require'neotest'.run.run(vim.fn.expand('%'))<CR>", "Run current file" },
   p = { "<CMD>lua require'neotest'.output.open({ enter = true, auto_close = true })<CR>", "Preview current test output" },
   o = { "<CMD>lua require'neotest'.output_panel.toggle()<CR>", "Toggle output panel" },
