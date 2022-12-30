b2cc29e3ec0261489c74159db4aa50947817eb28
Amaury Cannesson <amaury.cannesson@gmail.com>
2022-12-30 14:45:28 +0100

    lvim setup neotest

--- .config/lvim/lua/user/plugins.lua
+++ .config/lvim/lua/user/plugins.lua
@@ -3,5 +3,8 @@ lvim.plugins = {
     "Mofiqul/dracula.nvim",
     "fladson/vim-kitty",
     "mfussenegger/nvim-dap-python",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-neotest/neotest",
+    "nvim-neotest/neotest-python"
   },
 }
--- .config/lvim/lua/user/which-key.lua
+++ .config/lvim/lua/user/which-key.lua
@@ -5,3 +5,12 @@ lvim.builtin.which_key.mappings["s"]["b"] = {
 lvim.builtin.which_key.mappings["r"] = {
   ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>", "Replace"
 }
+
+lvim.builtin.which_key.mappings["t"] = {
+  name = "Neotest",
+  t = { "<CMD>lua require'neotest'.run.run()<CR>", "Run nearest test" },
+  f = { "<CMD>lua require'neotest'.run.run(vim.fn.expand('%'))<CR>", "Run current file" },
+  p = { "<CMD>lua require'neotest'.output.open({ enter = true, auto_close = true })<CR>", "Preview current test output" },
+  o = { "<CMD>lua require'neotest'.output_panel.toggle()<CR>", "Toggle output panel" },
+  s = { "<CMD>lua require'neotest'.summary.toggle()<CR>", "Toggle summary panel" },
+}
